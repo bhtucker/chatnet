180a625cd69b6cca12746f6ac05d639be4761ab5
Benson Tucker <benson@chatid.com>
2017-09-03 11:11:32 -0400

    more docstrings, comments, generality

--- chatnet/gather.py
+++ chatnet/gather.py
@@ -1,66 +1,31 @@
-import pandas as pd
-import os
-message_cols = []
-
-DROPBOX_DIR = os.getenv('DROPBOX_DIR')
-# NLP prep from excel:
-
-def _desirable_filename(filename):
-    return all([
-        filename.endswith('.xlsx'),
-        'Moderat' in filename,
-        'JH' not in filename])
-
-def get_mega_df():
-    dfs = []
-    for d, cts, files in os.walk(DROPBOX_DIR):
-        if 'Completed Transcripts' not in d:
-            continue
-        if len(files) == 1 and _desirable_filename(files[0]):
-            print files[0]
-            xls = pd.ExcelFile(os.path.join(d, files[0]))
-            dfs.append(xls.parse(xls.sheet_names[0]))
-    return pd.concat(dfs)
-
-def get_message_cols(df):
-    cols = [x for x in df.columns if x.startswith('Message')]
-    cols.sort(key=lambda v: int(v[v.find(' ') + 1:]))
-    return cols
+"""
+"""
 
 
-def assemble_messages(row, message_iterator, value_filter=lambda v: True, prepend_sender=False, valid_bodies=True):
+def assemble_messages(row, message_generator, value_filter=lambda v: True):
     sequence = []
-    for cell in message_iterator(row):
-        if isinstance(cell, (unicode, str)) and ':' in cell:
-            if value_filter(cell):
-                message = cell[cell.find(':') + 1:]
-
-                # optional message filters
-                if valid_bodies:
-                    if not message.strip():
-                        continue
-                if prepend_sender:
-                    sequence += ['$visitor' if cell.startswith('Visitor') else '$agent']
-                sequence += message.split()
+    for message in message_generator(row):
+        if isinstance(message, (unicode, str)) and value_filter(message):
+            sequence += message.split()
         else:
             return sequence
     return sequence
 
 
-def get_wide_columns_message_iterator(message_cols):
-    # Returns a function that gets a message iterator from a row
+def get_wide_columns_message_generator(message_cols):
+    # Returns a message generator
     # given a list of column names where messages can be found
-    def message_iterator(row):
+    def message_generator(row):
         for key in message_cols:
             yield row[key]
-    return message_iterator
+    return message_generator
 
-def get_dense_column_message_iterator(column):
-    # Returns a function that gets a message iterator from a row
+
+def get_dense_column_message_generator(column):
+    # Returns a function that gets a message generator from a row
     # given a column name which points to an array of messages
 
-    def message_iterator(row):
+    def message_generator(row):
         for cell in row[column]:
             yield cell
-    return message_iterator
-
+    return message_generator
--- chatnet/prep.py
+++ chatnet/prep.py
@@ -1,10 +1,20 @@
+"""
+Functionality for cleaning text inputs from the wild,
+constructing fixed-length word index sequences,
+and providing embeddings from GloVe word vectors
+"""
+
 import numpy as np
 from . import logger
-import re, string
-from collections import Counter
+import re
+import string
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
 
 numeric_pat = re.compile('.*[\d].*')
 caps_pat = re.compile('.*[A-Z].*')
+GLOVE_VEC_TEMPLATE = '/Users/bhtucker/rc/chatnet/glove.twitter.27B/glove.twitter.27B.{dimensions}d.txt'
+GLOVE_DIMS = {25, 50, 200}
 
 
 class TextPrepper(object):
@@ -17,25 +27,40 @@ class TextPrepper(object):
         self.exclude = exclude
 
     def cleaner(self, word):
+        """
+        Return :word with:self.exclude chars (ie punctuation) stripped from token
+        Group numeric inputs by ad hoc logic
+        Values returned prepended with '$' are always included in vocabulary (see get_word_index)
+        """
         if not numeric_pat.match(word):
             return ''.join(ch for ch in word.lower() if ch not in self.exclude)
         if '$' in word:
             return '$price'
         if '800' in word:
             return '$phone'
-        if 'www' in  word or 'http' in word:
+        if 'www' in word or 'http' in word:
             return '$web'
         if '-' in word or caps_pat.match(word):
             return '$model'
         else:
             return '$digit'
-    
-    def to_matrices(self, df, word_index, id_col='Chat Session ID', label_col='Chat Type',
-                     data_col='msgs', positive_class='product', seed=133, test_split=.2, **kwargs):
 
+    def to_matrices(self, df, word_index, id_col='Chat Session ID', label_col='Chat Type',
+                    data_col='msgs', positive_class='product', seed=133, test_split=.2, **kwargs):
+        """
+        Using :df and :word_index, return training and test data
+
+        Chunking and filtering will alter the number of rows.
+        :kwargs are passed along directly to self.chunk_tokens
+        """
         df = df[~df[id_col].isnull()]
         ids = df[id_col]
-        labels = df[label_col].map(lambda v: 1 if v == positive_class else 0)
+
+        if positive_class is None:
+            le = LabelEncoder()
+            labels = np_utils.to_categorical(le.fit_transform(df[label_col]))
+        else:
+            labels = df[label_col].map(lambda v: 1 if v == positive_class else 0)
         labels = zip(ids, labels)
         X = df[data_col].tolist()
 
@@ -43,6 +68,8 @@ class TextPrepper(object):
         np.random.shuffle(X)
         np.random.seed(seed)
         np.random.shuffle(labels)
+
+        # Split data before chunking to keep full observations separate
         test_ix = int(len(X) * (1 - test_split))
         X_train = np.array(X[:test_ix])
         labels_train = labels[:test_ix]
@@ -50,8 +77,8 @@ class TextPrepper(object):
         X_test = np.array(X[test_ix:])
         labels_test = labels[test_ix:]
 
-        X_train, labels_train = self.chunk_convos(X_train, labels_train, word_index, **kwargs)
-        X_test, labels_test = self.chunk_convos(X_test, labels_test, word_index, **kwargs)
+        X_train, labels_train = self.chunk_tokens(X_train, labels_train, word_index, **kwargs)
+        X_test, labels_test = self.chunk_tokens(X_test, labels_test, word_index, **kwargs)
 
         y_train = np.array([x[1] for x in labels_train])
         train_ids = [x[0] for x in labels_train]
@@ -61,66 +88,96 @@ class TextPrepper(object):
 
         return (X_train, y_train, train_ids), (X_test, y_test, test_ids)
 
-    def chunk_convos(self, X, labels, word_index, chunk_size=100,
+    def chunk_tokens(self, X, labels, word_index, chunk_size=100,
                      max_dummy_ratio=2, chunk_overlap_ratio=2):
+        """
+        Neural network models require dense, fixed width inputs
+        This function splits up and pads input text into :chunk_size length
+        lists of vocabulary index ints. Each observation's labels are duplicated
+        in parallel so that the returned numeric chunks representations are labeled
+
+        :X is a list of token lists
+        :labels is a list like (id, class label)
+        :word_index is a cleaned word : integer index vocabulary dict
+        :chunk_size is the guaranteed length of each row in return value :chunk_X
+        :max_dummy_ratio is a filter to remove messages with too many out of vocab chars
+            if a chunk has over (1/max_dummy_ratio) oov chars, it will not be returned
+        :chunk_overlap_ratio is the amount of overlapping text among sequential chunks
+            overlapping text gives the model more perspectives on sequences, and more training data
+            along with dummy filtering, you can select for higher signal inputs
+
+        Returns:
+            :chunk_X, :chunk_labels as the model-ready observations
+        """
         chunk_X, chunk_labels = [], []
         skipped = 0
         for x, label in zip(X, labels):
-            # x is the whole chat
+            # x is the whole observation
             l = len(x)
             clean_x = map(self.cleaner, x)
             index_representation = [
                 word_index[w] + self.index_from
-                    if w in word_index
-                    else self.oov_char
-                    for w in clean_x
-                ]
+                if w in word_index
+                else self.oov_char
+                for w in clean_x
+            ]
 
             chunk_idx = 0
             for start in (range(0, l - chunk_size, chunk_size / chunk_overlap_ratio) or [0]):
                 chunk_idx += 1
                 chunk = [self.start_char] + index_representation[start:start + chunk_size]
-                if not chunk:
-                    continue
-                pad_size = (chunk_size - len(chunk) + 1) #  add a one due to start_char
+                pad_size = (chunk_size - len(chunk) + 1)  # add a one due to start_char
                 padded = [self.pad_char] * pad_size + chunk
                 if (
                     sum(c == self.oov_char for c in padded) > ((chunk_size - pad_size) / max_dummy_ratio)
                     or
                     all([c in self.special_chars for c in chunk])
-                    ):                    
+                ):
                     skipped += 1
                     continue
                 else:
                     chunk_X.append(padded)
                     chunk_labels.append(('_'.join([label[0], str(chunk_idx)]), label[1]))
-                # break
+
         logger.info("Skipped %s for excess dummies" % skipped)
         return chunk_X, chunk_labels
 
 
-def get_word_counts(data, tp):
-    word_counts = Counter()
+def get_embedding_weights(word_index, index_from=3, embedding_size=200, zero_pad=0):
+    """
+    Read through GloVe word vectors for :embedding_size dimensions to get word embeddings
+    index_from specifies how many rows should be left for special characters
+    Returns:
+    :embedding_weights stack of word embedding rows keyed by :word_index + :index_from
+    :n_symbols number of rows of embedding_weights
+    """
+    n_symbols = len(word_index) + index_from
+    embedding_weights = np.zeros((n_symbols, embedding_size + zero_pad))
 
-    def increment(word):
-        word_counts[tp.cleaner(word)] += 1
+    def update_weights(line):
+        if line[:line.find(' ')] in word_index:
+            tokens = line.split()
+            embedding_weights[word_index[tokens[0]] + index_from] = \
+                np.array(map(np.float, tokens[1:] + [0] * zero_pad))
 
-    data.map(lambda r: map(increment, r))
+    with open(get_vec_file(embedding_size), 'r') as f:
+        map(update_weights, f)
 
-    return word_counts
+    return embedding_weights, n_symbols
 
 
-def get_word_index(word_counts, nb_words=15000, skip_top=None, nonembeddable=None):
-    skip_top = skip_top or 10
+def get_vec_file(dimensions):
+    return GLOVE_VEC_TEMPLATE.format(dimensions=dimensions)
 
-    vocab = []
-    for (ix, (w, _)) in enumerate(word_counts.most_common(nb_words + skip_top)):
-        if w.startswith('$'):
-            if ix < skip_top:
-                skip_top += 1
-            vocab.append(w)
-        elif (not nonembeddable or not w in nonembeddable) and ix > skip_top:
-            vocab.append(w)
 
-    return {v: ix for ix, v in enumerate(vocab)}
-
+def get_nonembeddable_set(word_counts, rank_cutoff=50000):
+    """
+    Returns set of top :rank_cutoff words without GloVe embeddings
+    """
+    full_vocab = set(w for w, _ in word_counts.most_common(rank_cutoff))
+    seen = set()
+    with open(get_vec_file(list(GLOVE_DIMS)[0]), 'r') as f:
+        for line in f:
+            if line[:line.find(' ')] in full_vocab:
+                seen.add(line[:line.find(' ')])
+    return full_vocab - seen
